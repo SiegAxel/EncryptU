e58af878a4bbf9b393a09efd5681f8bdd11ff47c
SiegAxel <f.torresa@hotmail.com>
2025-10-11 23:30:36 -0300

    fix

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Habilitar Webpack para resolver alias
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 export default nextConfig;
